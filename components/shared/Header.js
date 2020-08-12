c1531b6cb4929401bbfefcc0bd5a0eead3025d12
arturfil <arturfil@hotmail.com>
2020-08-12 03:56:21 -0400

    added the cookies to auth 0

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -65,6 +65,9 @@ const Header = ({user, loading}) =>  {
             <NavItem className="port-navbar-item">
               <BsNavLink href="/secret" title="Secret"/>
             </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/secretssr" title="SecretSSR"/>
+            </NavItem>
           </Nav>
           <Nav className="navbar">
             { !loading && 
--- pages/secretssr.js
+++ pages/secretssr.js
@@ -1,31 +1,30 @@
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
-import auth0 from '../utils/auth0';
+import {authorizeUser, withAuth} from '../utils/auth0';
 
-const SecretSSR = ({user}) => {
+const SecretSSR = ({user, title}) => {
   return (
     <BaseLayout user={user} loading={false}>
       <BasePage>
         <h1>I am secret page with SSR (Server Side Rendering)</h1>
         <h2>Welcome {user && user.name}</h2>
+        <h2>{title}</h2>
       </BasePage>
     </BaseLayout>
   )
 }
 
-export const getServerSideProps = async ({req, res}) => {
-  const session = await auth0.getSession(req);
-  if (!session || !session.user) {
-    res.writeHead(302, {
-      Location: '/api/v1/login'
-    });
-    res.end();
-    return {props: {}};
-  }
-
-  return {
-    props: { user: session.user}
-  }
+const getTitle = () => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res({title: "Returning new Title"}) 
+    }, 500)
+  })
 }
+
+export const getServerSideProps = withAuth(async () => {
+  const title = await getTitle();
+  return title;
+});
 
 export default SecretSSR;
