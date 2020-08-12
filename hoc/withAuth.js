714afb016c9716d08fede19068395ec0ff130131
arturfil <arturfil@hotmail.com>
2020-08-12 03:54:29 -0400

    added onlyadmin page

--- hoc/withAuth.js
+++ hoc/withAuth.js
@@ -1,7 +1,7 @@
 import { useGetUser } from '../actions/user';
 import Redirect from '../components/shared/Redirect';
 
-const withAuth = Component => props => {
+const withAuth = Component => role => {
   const {data, loading} = useGetUser();
 
   if (loading) {
@@ -9,8 +9,11 @@ const withAuth = Component => props => {
   }
   if (!data) 
     return <Redirect ssr to="/api/v1/login" />;
-  else 
+  else {
+    if (data && !data[process.env.AUTH0_NAMESPACE + '/roles'].includes(role))
+      return <Redirect ssr to ="api/v1/login"/>;
     return <Component data={data} loading={loading} {...props} />;
+  }
 }
 
 export default withAuth;
