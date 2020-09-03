fdc1ab2b6fddd0fbfefc1496571a1df1c09037fb
arturfil <arturfil@hotmail.com>
2020-09-03 19:54:44 -0400

    refactored the createProject method in api/v1 in order to receive data

--- pages/api/v1/projects.js
+++ pages/api/v1/projects.js
@@ -5,7 +5,7 @@ const createProject = async (req, res) => {
   try {
     const {accessToken} = await auth0.getSession(req);
     const createdProject = await new ProjectApi(accessToken).createProject(req.body);
-    return res.json(createdProject);
+    return res.json(createdProject.data);
   } catch (error) {
     return res.status(error.status||400).end(error.message);
   }
